25f10b1d415aae158fe47813c8a7ab58684b2b6f
salman-41 <salman.yz@outlook.com>
2023-10-29 22:08:40 +0500

    light and dark mode added

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ const nav = document.getElementById("nav");
 const toggleIcon = document.getElementById("toggle-icon");
 const image1 = document.getElementById("image1");
 const image2 = document.getElementById("image2");
-const image = document.getElementById("image3");
+const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
+
+// Dark or Light Images
+const imageMode = function (color) {
+  image1.src = `img/undraw_conceptual_idea_${color}.svg`;
+  image2.src = `img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `img/undraw_proud_coder_${color}.svg`;
+};
 
 // Dark Mode
 function darkMode() {
@@ -12,6 +19,16 @@ function darkMode() {
   textBox.style.backgroundColor = "rgb(255 255 255 / 50%";
   toggleIcon.children[0].textContent = "Dark Mode";
   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+  imageMode("dark");
+}
+
+// Light Moon
+function lightMode() {
+  nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  toggleIcon.children[0].textContent = "Light Mode";
+  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  imageMode("light");
 }
 
 // Switch Theme Dynamically
