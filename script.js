2ca2d910f6aa6e66ea52d23795a87ddbbe60a8d6
salman-41 <salman.yz@outlook.com>
2023-10-29 22:00:11 +0500

    darkMode

--- script.js
+++ script.js
@@ -6,12 +6,22 @@ const image2 = document.getElementById("image2");
 const image = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
 
+// Dark Mode
+function darkMode() {
+  nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
+  textBox.style.backgroundColor = "rgb(255 255 255 / 50%";
+  toggleIcon.children[0].textContent = "Dark Mode";
+  toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
+}
+
 // Switch Theme Dynamically
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    darkMode();
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    lightMode();
   }
 }
 //Event Listener
